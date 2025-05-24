10a3a018c8353ed3427660154bd87e8da64588cb
n6021n <n6021n@icloud.com>
2025-05-23 22:07:01 -0500

    Menu and visual enhancements

--- script.js
+++ script.js
@@ -11,17 +11,21 @@ function doHeader(ActiveItem) {
     const header = document.querySelector('header');
     const headerHTML = `
         <div class="container-navbar">
-            <div class="logo">
-                <a href="index.html">E.R.I. Krueger</a>
-            </div>
+
+        <div style="display: flex;">
             <div class="container-navbar-toggle">
                 <button class="navbar-toggle" onclick="toggleMenu()">☰</button>
             </div>
+            <div class="logo">
+                <a href="index.html">E.R.I. Krueger</a>
+            </div>
+        </div>
+
             <nav>
                 <ul>
                     <li><a href="index.html" id="index">Home</a></li>
                     <li><a href="about.html" id="about">About</a></li>
-                    <li><a href="writing.html" id="projects">Projects</a></li>
+                    <li><a href="writing.html" id="writing">Writing</a></li>
                     <li><a href="contact.html" id="contact">Contact</a></li>
                 </ul>
             </nav>
